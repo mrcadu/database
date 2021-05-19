dd0478e97a10bb1553d93d3b3b8e5fbdadfc98a6
ncorreia <ncorreia.ufrj@gmail.com>
2021-05-19 12:09:28 -0700

    Adiciona método que popula database.

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include "SqlCompiler.h"
 #include "InputBuffer.h"
 
+void populateDatabase();
+
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
@@ -11,9 +13,47 @@ int main(int argc, char* argv[])
     }
     char* filename = argv[1];
     Table* table = getTable(filename);
+    populateDatabase(table);
+    printf(" database populated! \n");
     while(true){
         InputBuffer* input_buffer = new_input_buffer();
         readSql(input_buffer, table);
     }
     return 0;
+}
+
+void populateDatabase(Table* table)
+{
+    FILE *the_file = fopen("database.csv", "r");
+    if(the_file == NULL){
+        perror("Unable to open the file");
+        exit(1);
+    }
+    char line[200];
+    while(fgets(line, sizeof(line), the_file)){
+        char *token;
+        token = strtok(line, ",");
+        Statement statement;
+        Row row_to_insert;
+        int count = 0;
+
+        while(token != NULL){
+            switch (count) {
+                case 0:
+                    row_to_insert.id = atoi(token);
+                    count++;
+                    break;
+                case 1:
+                    strcpy(row_to_insert.username, token);
+                    count++;
+                    break;
+                case 2:
+                    strcpy(row_to_insert.email, token);
+                    break;
+            }
+            token = strtok(NULL,",");
+        }
+        statement.row_to_insert = row_to_insert;
+        execute_insert(&statement, table);
+    }
 }
